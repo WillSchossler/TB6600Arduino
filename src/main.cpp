8f09141f2a960e84238622c48680f08aa04d9870
William Gabriel Schossler <gabrielschossler.principal@gmail.com>
2026-03-20 13:20:30 -0300

    Factored

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #define IN_POT A0  
-#define IN_ENA 10
 #define IN_DIR 9
 #define IN_START 8
 #define OUT_ENA 7
@@ -10,43 +9,34 @@
 int pot;
 int delayStep;
 bool started;
-bool enabled;
 bool direction;
 
 void setup() {  
   pinMode(IN_START, INPUT_PULLUP);
-  pinMode(IN_ENA, INPUT_PULLUP);
   pinMode(IN_DIR, INPUT_PULLUP);
   pinMode(OUT_ENA, OUTPUT);
   pinMode(OUT_DIR, OUTPUT);
   pinMode(OUT_PUL, OUTPUT);
-}
+
+  digitalWrite(OUT_ENA, HIGH);
+  }
 
 void loop() {
   started = digitalRead(IN_START);
-  enabled = digitalRead(IN_ENA); 
   direction = digitalRead(IN_DIR);
 
-  if (!enabled) {
-    digitalWrite(OUT_ENA, HIGH);
+  if (!started) {
     digitalWrite(OUT_DIR, direction);
-    if (!started) {
-      pot = analogRead(IN_POT);
-      delayStep = map(pot, 0, 1023, 2000, 200); 
+    pot = analogRead(IN_POT);
+    delayStep = map(pot, 0, 1023, 2000, 200); 
 
-      digitalWrite(OUT_PUL, HIGH);
-      delayMicroseconds(delayStep);
+    digitalWrite(OUT_PUL, HIGH);
+    delayMicroseconds(delayStep);
 
-      digitalWrite(OUT_PUL, LOW);
-      delayMicroseconds(delayStep);
+    digitalWrite(OUT_PUL, LOW);
+    delayMicroseconds(delayStep);
     }
     else {
       digitalWrite(OUT_PUL, LOW);
-    }
-    
+    }    
   }
-  else {
-    digitalWrite(OUT_PUL, LOW);
-    digitalWrite(OUT_ENA, LOW);
-  }
-}
